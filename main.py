c89aed9901d2515f86a15b6c59745833262b0558
Alexander Terbeznik <44547684+trbznk@users.noreply.github.com>
2023-09-15 09:25:30 +0200

    implement test loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import torch
+import argparse
 import matplotlib.pyplot as plt
 
 from torch import nn
@@ -22,8 +23,10 @@ class RandomTimeSeriesWindows(Dataset):
 
 
 class Model(nn.Module):
-    def __init__(self, window_size):
+    def __init__(self, window_size, max_marker):
         super().__init__()
+
+        self.max_marker = max_marker
 
         hidden_size = 8
         self.m = nn.Sequential(
@@ -33,68 +36,66 @@ class Model(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_size, window_size)
         )
-        self.a = nn.Sigmoid()
+        self.a = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.m(x)
         x = self.a(x)
         x.data[:, 0] = 1
         x.data[:, -1] = 1
+
+        # NOTE: this is probably very slow @Speedup
+        top_values, top_indices = torch.topk(x, self.max_marker)
+        top_values = top_values.min(axis=1).values
+        top_values = top_values.view(-1, 1)
+
+        # TODO: maybe we dont need this here but its better to test it
+        #       in a much simpler example
+        # top_values = top_values.repeat(1, 100)
+
+        x.data[x >= top_values] = 1
+        x.data[x < top_values] = 0
+
         return x
     
 
+def generate_decomp(x, mask):
+    output = x*mask
+    for i in range(output.shape[0]):
+        for j in range(output.shape[1]):
+            if mask[i, j] == 1:
+                left_index = j
+                left_value = output[i, j]
+            else:
+                # we know that we have a almost right corner with 1
+                # so we dont need any checks here to avoid index overflow
+                for k in range(j+1, output.shape[1]):
+                    if mask[i, k] == 1:
+                        right_index = k
+                        right_value = output[i, k]
+
+                        # stop after finding right neighbor
+                        break
+
+                b = left_value
+                m = (right_value-left_value) / (right_index-left_index)
+
+                current_delta = j-left_index
+
+                # new value
+                output[i, j] = m*current_delta + b
+    return output
+
+
 def train_loop(dataloader, model, loss_fn, optimizer):
     model.train()
+
     running_loss = 0.0
-
     for x in dataloader:
-        output = model(x)
-        mask = output.round()
-        output = x*mask
+        mask = model(x)
+        output = generate_decomp(x, mask)
 
-        # fig, axs = plt.subplots(4, figsize=(10, 6))
-        # for i in range(4):
-        #     axs[i].set_ylim(-1, 1)
-
-        for i in range(output.shape[0]):
-            # axs[0].plot(input[i].detach())
-            # axs[0].set_title("input")
-            # axs[1].plot(mask[i].detach())
-            # axs[1].set_title("mask")
-            # axs[2].plot(output[i].detach())
-            # axs[2].set_title("output masked")
-
-            for j in range(output.shape[1]):
-                if mask[i, j] == 1:
-                    left_index = j
-                    left_value = output[i, j]
-                else:
-                    # we know that we have a almost right corner with 1
-                    # so we dont need any checks here to avoid index overflow
-                    for k in range(j+1, output.shape[1]):
-                        if mask[i, k] == 1:
-                            right_index = k
-                            right_value = output[i, k]
-
-                            # stop after finding right neighbor
-                            break
-
-                    b = left_value
-                    m = (right_value-left_value) / (right_index-left_index)
-
-                    current_delta = j-left_index
-
-                    # new value
-                    output[i, j] = m*current_delta + b
-
-            # axs[3].plot(output[i].detach())
-            # axs[3].set_title("output interpolated")
-            # plt.show()
-
-        alpha = 0.1
         loss = loss_fn(x, output)
-        loss = loss + mask*alpha
-        loss = loss.mean()
 
         loss.backward()
         optimizer.step()
@@ -104,26 +105,62 @@ def train_loop(dataloader, model, loss_fn, optimizer):
     return running_loss/len(dataloader)
 
 
-WINDOW_SIZE = 8
-BATCH_SIZE = 16
-EPOCHS = 100
+def test_loop(dataloader, model, loss_fn):
+    model.eval()
 
-train_data = RandomTimeSeriesWindows(100, WINDOW_SIZE)
-test_data = RandomTimeSeriesWindows(20, WINDOW_SIZE)
-train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE)
-test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE)
-model = Model(WINDOW_SIZE)
-loss_fn = nn.MSELoss(reduction="none")
-optimizer = torch.optim.Adam(model.parameters())
+    with torch.no_grad():
+        for x in dataloader:
+            mask = model(x)
+            output = generate_decomp(x, mask)
 
-for epoch in range(EPOCHS):
-    loss = train_loop(train_dataloader, model, loss_fn, optimizer)
-    print(f"{(epoch+1):2}: loss={loss:.4f}")
+            loss = loss_fn(x, output)
+            
+            fig, ax = plt.subplots(1, figsize=(10, 6))
+            ax.set_ylim(-1.5, 1.5)
+
+            window = x.view(-1)
+            window_decomposed = output.view(-1)
+            amount_markers = int(mask.sum().item())
+            markers = (x*mask).view(-1)
+            markers[markers == 0] = float("nan")
+
+            ax.set_title(f"markers={amount_markers}")
+            ax.plot(window)
+            ax.plot(window_decomposed)
+            ax.plot(markers, "k.")
+            plt.show()
 
 
+if __name__ == "__main__":
+    WINDOW_SIZE = 100
+    BATCH_SIZE = 16
+    EPOCHS = 5
+    MAX_MARKER = 10
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--train", action="store_true")
+    parser.add_argument("-p", "--predict", action="store_true")
+    args = parser.parse_args()
 
+    train_data = RandomTimeSeriesWindows(100, WINDOW_SIZE)
+    test_data = RandomTimeSeriesWindows(5, WINDOW_SIZE)
+    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE)
+    test_dataloader = DataLoader(test_data, batch_size=1)
+    model = Model(WINDOW_SIZE, MAX_MARKER)
+    loss_fn = nn.MSELoss()
 
+    if args.train:    
+        optimizer = torch.optim.Adam(model.parameters())
 
+        for epoch in range(EPOCHS):
+            loss = train_loop(train_dataloader, model, loss_fn, optimizer)
+            print(f"{(epoch+1):2}: loss={loss:.4f}")
 
+        torch.save(model, "model.pth")
 
+    if args.predict:
+        model = torch.load("model.pth")
+        loss = test_loop(test_dataloader, model, loss_fn)
+
+    if not args.predict and not args.train:
+        parser.print_help()
